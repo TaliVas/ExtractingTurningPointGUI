19ebe651afea88d1cbec72faa3f750cc8e119c00
TaliVas <avital7772@gmail.com>
2024-09-01 17:50:36 +0300

    condensed functionality into .py

---
+++ utils/feature_calc.py
@@ -0,0 +1,264 @@
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+from scipy.signal import savgol_filter
+from sklearn.preprocessing import StandardScaler
+
+def data_preprocessing(df, min_len):
+    df.rename(columns={'DLC_color_id':'id'}, inplace=True)
+    df = df.dropna(subset=['time_milisecond', 'id'])
+    df = df.groupby('id').filter(lambda x: len(x) >= min_len)
+    return df
+
+def process_dataframe(len_df):
+    """
+    Create processed df.
+    """
+    process_df = []
+    for trial_id in len_df['id'].unique():
+        trial = process_trial(len_df, trial_id)
+        process_df.append(trial)
+    process_df = pd.concat(process_df, ignore_index=True)
+    return process_df
+
+def process_trial(df, trial_id):
+    """
+    Process a single trial by updating columns, computing velocity, and updating movement times.
+    """
+    trajectory = df[df['id'] == trial_id].copy() #all data points with the same id
+    initial_start_time = trajectory.iloc[0]['start_time']
+    
+    # Adjust time columns
+    time_columns = ['start_time', 'end_time', 'cue_time', 'go_time', 'update_time', 'move_time','time_milisecond']
+    for col in time_columns:
+        trajectory[col] -= initial_start_time
+    trajectory['time_milisecond'] *= 1000
+    trajectory['cue_milisecond'] = trajectory.iloc[0]['cue_time'] * 1000
+    trajectory['go_milisecond'] = trajectory.iloc[0]['go_time'] * 1000
+    trajectory = compute_velocities(trajectory, trajectory.iloc[0]['cue_time'] * 1000)
+    return trajectory
+
+
+def compute_velocities(df, signal_cue):
+    """
+    Calculate velocity for movements post-signal cue.
+    """
+    # Filter and calculate differences
+    df_filtered = df[df['time_milisecond'] >= signal_cue].copy()
+    dx = df_filtered['x'].diff()
+    dy = df_filtered['y'].diff()
+    dt = df_filtered['time_milisecond'].diff()
+    
+    # Compute velocity while avoiding division by zero
+    df_filtered['velocity'] = np.sqrt(dx**2 + dy**2) / (dt + 1e-10)
+    ddx = dx.diff().fillna(0)
+    ddy = dy.diff().fillna(0)
+    df_filtered['curvature'] = (dx * ddy - dy * ddx) / (np.power(dx ** 2 + dy ** 2, 1.5) + 1e-10)
+
+    return df_filtered.iloc[1:]  # skip the first NaN result from diff()(0)
+
+def window_and_normalize_trial(trial, right_window=1500, left_window=200):
+    """
+    Process and window a single trial.
+    """
+    signal_go = trial.iloc[0]['go_milisecond']
+
+    # Determine the window around the go signal
+    window_start = signal_go - left_window
+    window_end = signal_go + right_window
+
+    # Filter the trial to the specified window
+    windowed_trial = trial[(trial['time_milisecond'] >= window_start) & (trial['time_milisecond'] <= window_end)].copy()
+    new_start_time = windowed_trial.iloc[0]['time_milisecond']
+
+    # Adjust times to start from zero within the window
+    windowed_trial['adjusted_time'] = windowed_trial['time_milisecond'] - new_start_time
+
+    # Normalize the adjusted times
+    max_adjusted_time = windowed_trial['adjusted_time'].max()
+    windowed_trial['normalized_time'] = windowed_trial['adjusted_time'] / max_adjusted_time
+
+    return windowed_trial
+
+def window_trials(processed_df, right_window=1500, left_window=200):
+    """
+    Adjust times for each trial in the processed DataFrame.
+    """
+    # Apply the function to all trials in the dataframe
+    windowed_trials = []
+    for trial_id in processed_df['id'].unique():
+        trial = processed_df[processed_df['id'] == trial_id]
+        windowed_trial = window_and_normalize_trial(trial, right_window, left_window)
+        windowed_trials.append(windowed_trial)
+
+    return pd.concat(windowed_trials, ignore_index=True)
+
+def apply_smoothing(group, col_x='x', col_y='y', window_length=5, poly_order=1):
+    """
+    Applies Savitzky-Golay smoothing to specified columns in a DataFrame group.
+    """
+    # Apply the Savitzky-Golay filter
+    x_smooth = savgol_filter(group[col_x], window_length, poly_order)
+    y_smooth = savgol_filter(group[col_y], window_length, poly_order)
+
+    # Add smoothed data to new columns
+    group['smooth_x'] = x_smooth
+    group['smooth_y'] = y_smooth
+
+    return group
+
+def recenter_trajectory(group):
+    """
+    Recenters a trajectory group by subtracting the coordinates at the start movement time.
+    """
+
+    initial_x_offset = group['smooth_x'].iloc[0]
+    initial_y_offset = group['smooth_y'].iloc[0]
+
+    # Apply the offset to recenter the trajectory
+    group['centered_x'] = group['smooth_x'] - initial_x_offset
+    group['centered_y'] = group['smooth_y'] - initial_y_offset
+
+    return group
+
+def apply_rotation(group):
+    """
+    Applies rotation and reflection transformations to trajectory data.
+    """
+    # Extract target ID from the first row of the group
+    id_target = group.iloc[0]['id_target']
+    # Initialize target angles for rotation
+    target_angles = {1: 270, 2: 315, 3: 0, 4: 45, 5: 90, 6: 135, 7: 180, 8: 225}
+
+    # Check for updates and obtain update target ID if present
+    is_updated = not pd.isna(group.iloc[0]['id_update'])
+    id_target_update = group.iloc[0]['id_update'] if is_updated else None
+
+    if id_target == 5:
+        if is_updated and (id_target_update > id_target):
+            # Reflect over the Y-axis if movement is not clockwise
+            group['rotation_x'],group['rotation_y']  = -group['centered_x'], group['centered_y']
+        else:
+            # No rotation needed
+            group['rotation_x'], group['rotation_y'] = group['centered_x'], group['centered_y']
+    else:
+        # General case for all other targets
+        rotation_angle = 90 - target_angles[id_target] 
+        # Rotate coordinates based on computed angle
+        group['rotation_x'], group['rotation_y'] = rotate_trajectory(group['centered_x'], group['centered_y'], rotation_angle)
+        if is_updated and (id_target_update > id_target):
+            # Reflect over the Y-axis if movement is not clockwise
+            group['rotation_x'], group['rotation_y'] =  -group['centered_x'], group['centered_y']
+    return group
+
+def rotate_trajectory(x, y, angle):
+    """
+    Rotates a point around the origin (0, 0) by a specified angle.
+    """
+    angle_rad = np.radians(angle)
+    x_new = x * np.cos(angle_rad) - y * np.sin(angle_rad)
+    y_new = x * np.sin(angle_rad) + y * np.cos(angle_rad)
+    return x_new, y_new
+def map_targets_to_angle(id_target, id_update):
+    """
+    Maps target IDs to angles.
+    """
+    if not pd.isna(id_update):
+        old_target, new_target = round(id_target), round(id_update)
+        if not np.isnan(new_target):
+            target_diff = abs(new_target - old_target)
+            # Adjust for circular nature of targets.
+            if target_diff > 4:
+                target_diff = 8 - target_diff
+            angle_map = {1: 45, 2: 90, 3: 135, 4: 180}
+            return angle_map.get(target_diff, 0)
+    else:
+        return 0
+    
+def calc_points(group):
+    """
+    Calculate the angles between consecutive triplets of points in the group.
+    """
+    group['point'] = list(zip(group['rotation_x'], group['rotation_y']))
+    angles = []
+    for i in range(len(group) - 2):
+        A = group['point'].iloc[i]
+        B = group['point'].iloc[i + 1]
+        C = group['point'].iloc[i + 2]
+        angle = angle_between_three_points(A, B, C)
+        angles.append(angle)
+    # Add NaN for the last two points as they don't have enough subsequent points for the angle calculation
+    angles.extend([np.nan, np.nan])
+    group['angle'] = angles
+    return group
+
+def angle_between_three_points(A, B, C):
+    # Convert points to numpy arrays
+    A = np.array(A)
+    B = np.array(B)
+    C = np.array(C)
+    # Calculate vectors AB and BC
+    AB = B - A
+    BC = C - B
+    # Calculate the dot product and magnitudes of AB and BC
+    dot_product = np.dot(AB, BC)
+    magnitude_AB = np.linalg.norm(AB)
+    magnitude_BC = np.linalg.norm(BC)
+    # Calculate the cosine of the angle
+    cos_angle = dot_product / (magnitude_AB * magnitude_BC)
+    # Handle potential numerical issues
+    cos_angle = np.clip(cos_angle, -1.0, 1.0)
+    # Calculate the angle in radians
+    angle_radians = np.arccos(cos_angle)
+    # Convert the angle to degrees
+    angle_degrees = np.degrees(angle_radians)
+    return angle_degrees
+
+def calculate_relative_angle_to_end(group):
+    end_x, end_y = group['rotation_x'].iloc[-1], group['rotation_y'].iloc[-1]
+    delta_x_to_end = end_x - group['rotation_x']
+    delta_y_to_end = end_y - group['rotation_y']
+    
+    start_x, start_y = group['rotation_x'].iloc[0], group['rotation_y'].iloc[0]
+    delta_x_start_to_end = end_x - start_x
+    delta_y_start_to_end = end_y - start_y
+    
+    angle_to_end = np.arctan2(delta_y_to_end, delta_x_to_end)
+    angle_start_to_end = np.arctan2(delta_y_start_to_end, delta_x_start_to_end)
+    relative_angles = np.degrees(angle_to_end - angle_start_to_end)
+    relative_angles = (relative_angles + 180) % 360 - 180
+    group['angle_to_end'] = relative_angles
+    group['distance_to_end'] = np.sqrt((group['rotation_x'] - group['rotation_x'].iloc[-1])**2 + (group['rotation_y'] - group['rotation_y'].iloc[-1])**2)
+    return group
+
+def scale_features(df, features, group_col):
+    """
+    Scale specified features within each group defined by group_col.
+    """
+    df_final = df.copy()
+    scaler = StandardScaler()
+    for feature in features:
+        scaled_feature = 'scaled_' + feature
+        df_final[scaled_feature] = df_final.groupby(group_col)[feature].transform(lambda group: scaler.fit_transform(group.values.reshape(-1, 1)).flatten())
+    return df_final
+
+def feature_calc(df):
+    len_df = data_preprocessing(df, min_len=10)
+    processed_df = process_dataframe(len_df)
+    df_windowed = window_trials(processed_df)
+    df_smooth = df_windowed.groupby('id').apply(apply_smoothing)
+    df_smooth = df_smooth.reset_index(drop=True)
+    df_centered = df_smooth.groupby('id').apply(recenter_trajectory)
+    df_centered.reset_index(drop=True, inplace=True)
+    df_rotation = df_centered.groupby('id').apply(apply_rotation)
+    df_rotation = df_rotation.reset_index(drop=True)
+    df_rotation['type_trajectory'] = df_rotation.apply(lambda row: map_targets_to_angle(row['id_target'], row['id_update']), axis=1)
+    df_features_angle = df_rotation.groupby('id').apply(calc_points)
+    df_features_angle.reset_index(drop=True, inplace=True)
+    df_features_angle_to_end = df_features_angle.groupby('id').apply(calculate_relative_angle_to_end)
+    df_features_angle_to_end.reset_index(drop=True, inplace=True)
+    features_to_scale = ['rotation_x', 'rotation_y','velocity', 'angle', 'angle_to_end', 'curvature','distance_to_end']
+    df_final = scale_features(df_features_angle_to_end, features_to_scale, 'type_trajectory')
+    df_features_final = df_final.dropna(axis=0, subset=features_to_scale)
+    return df_features_final
---
+++ utils/predictions.py
@@ -0,0 +1,219 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+from utils.convert_raw_to_df import convert_raw_to_df
+from utils.feature_calc import feature_calc
+
+class TrajectoryDataset(Dataset):
+    def __init__(self, sequences, types, labels):
+        self.sequences = torch.tensor(sequences, dtype=torch.float32)
+        self.types = torch.tensor(types, dtype=torch.int64)
+        self.labels = torch.tensor(labels, dtype=torch.float32)
+        
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        return self.sequences[idx], self.types[idx], self.labels[idx]
+    
+class TrajectoryModel(nn.Module):
+    def __init__(self, num_trajectory_types, number_of_features, sequence_length):
+        super(TrajectoryModel, self).__init__()
+        self.lstm = nn.LSTM(number_of_features, 50, bidirectional=True, batch_first=True)
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.embedding = nn.Embedding(num_trajectory_types, 2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(100 + 2, 100)  # 100 from LSTM + 2 from embedding
+        self.fc2 = nn.Linear(100, 1)
+        
+    def forward(self, x, type_input):
+        lstm_out, _ = self.lstm(x)
+        lstm_out = lstm_out.permute(0, 2, 1)  # (batch_size, num_features, seq_len)
+        avg_pool_out = self.avg_pool(lstm_out).squeeze(-1)  # (batch_size, 100)
+        embedded_type = self.embedding(type_input)
+        embedded_type = self.flatten(embedded_type)
+        combined = torch.cat((avg_pool_out, embedded_type), dim=1)
+        x = torch.relu(self.fc1(combined))
+        x = self.fc2(x)
+        return x
+    
+def pad_sequences(sequences, maxlen, dtype='float32', padding='post', value=0):
+    padded_sequences = np.full((len(sequences), maxlen, len(sequences[0][0])), value, dtype=dtype)
+    for i, seq in enumerate(sequences):
+        if len(seq) > maxlen:
+            padded_sequences[i] = seq[:maxlen]
+        else:
+            padded_sequences[i, :len(seq)] = seq
+    return padded_sequences
+
+def make_predictions(df_test_features, models, model_name, features, model_path):
+    # Extract sequences from the DataFrame
+    X = df_test_features.drop(['type_trajectory'], axis=1)
+    grouped = X.groupby('id')
+    trajectories = [group[features].values for _, group in grouped]
+    ids = list(grouped.groups.keys())
+    trajectory_type = df_test_features.groupby('id')['type_trajectory'].first().values
+
+    # Check the unique values of trajectory_type
+    unique_trajectory_types = np.unique(trajectory_type)
+    trajectory_type_to_index = {t: i for i, t in enumerate(unique_trajectory_types)}
+    trajectory_type_indices = np.array([trajectory_type_to_index[t] for t in trajectory_type])
+    # Pad the sequences
+    max_sequence_length = max([len(sequence) for sequence in trajectories])
+    trajectories_padded = pad_sequences(trajectories, max_sequence_length, padding='post')
+
+    sequence_length = trajectories_padded.shape[1]
+    number_of_features = trajectories_padded.shape[2]
+
+    # Convert the data to PyTorch tensors
+    test_sequences_tensor = torch.tensor(trajectories_padded, dtype=torch.float32)
+    test_types_tensor = torch.tensor(trajectory_type_indices, dtype=torch.long)
+    # models = 'start_move_model.pth'
+    # model_name = {'start_move_model.pth': 'start_move'}
+
+    # Initialize an empty DataFrame for the result
+    df_predicted = pd.DataFrame({'id': ids })
+
+    # Initialize and load the model
+    model = TrajectoryModel(len(unique_trajectory_types), number_of_features, sequence_length)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    # Initialize a list to store predictions
+    predictions = []
+
+    # Make predictions using the model
+    with torch.no_grad():
+        outputs = model(test_sequences_tensor, test_types_tensor)
+        predictions.extend(outputs.squeeze().tolist())
+
+    # Convert each label's predictions into a DataFrame
+    column_name = model_name[models]
+    df_temp = pd.DataFrame({column_name: predictions, 'id': ids})
+
+    # Merge the temporary DataFrame with the main annotated DataFrame
+    if df_predicted.empty:
+        df_predicted = df_temp
+    else:
+        df_predicted = df_predicted.merge(df_temp, on='id', how='outer')
+
+    # Drop rows with missing values
+    df_predicted.dropna(inplace=True)
+
+    return df_predicted
+
+def edit_predictions(df_predicted, df_test_features, col_name):
+    # Merge df_predicted with df_test_features to add recording_day, file_id, and video_in_file_id
+    df_test_features_copy = df_test_features.copy()
+    df_predicted = df_predicted.merge(df_test_features_copy[['recording_day', 'file_number', 'trial_number_in_file','id']], on='id', how='left')
+    df_predicted = df_predicted.drop_duplicates(subset='id')
+
+    df_test_features_copy['adjusted_time'] = df_test_features_copy.groupby('id')['time_milisecond'].transform(lambda x: x - x.min())
+    df_test_features_copy['max_adjusted_time'] = df_test_features_copy.groupby('id')['adjusted_time'].transform('max')
+    df_test_features_copy['new_start_time'] = df_test_features_copy.groupby('id')['time_milisecond'].transform('min')
+
+    # Drop duplicates to keep only one row per id with the calculated values
+    df_test_features_unique = df_test_features_copy[['id', 'max_adjusted_time', 'new_start_time']].drop_duplicates()
+
+    # Merge df_predicted with df_test_features_unique to get the necessary columns
+    df_predicted = df_predicted.merge(df_test_features_unique, on='id', how='left')
+
+    # Revert the normalized time back to seconds
+    df_predicted[f'{col_name}_in_seconds'] = (df_predicted[col_name] * df_predicted['max_adjusted_time'] + df_predicted['new_start_time']) / 1000.0
+
+    # Drop the temporary columns used for the calculation
+    df_predicted.drop(columns=['max_adjusted_time', 'new_start_time'], inplace=True)
+
+    # Drop the original 'start_move' column
+    df_predicted.drop(columns=[col_name], inplace=True)
+
+    return df_predicted
+
+
+def plot_trajectory(df_predicted, df_test_features):
+    # Randomly select 10 unique ids from the test data
+    random_indices = random.sample(sorted(df_test_features['id'].unique()), 10)
+
+    # Create subplots
+    fig, axes = plt.subplots(5, 2, figsize=(8, 20))  
+    axes = axes.flatten()
+
+    for i, idx in enumerate(random_indices):
+        ax = axes[i]
+        # Extract the DataFrame for the current trajectory
+        df = df_test_features[df_test_features['id'] == idx]
+        
+        # Plot the trajectory
+        ax.plot(df['scaled_rotation_x'], df['scaled_rotation_y'], marker='o', alpha=0.5, markersize=3)
+        
+        # Use the 'id' to get the predicted start move time
+        predicted_time_start = df_predicted.loc[df_predicted['id'] == idx, 'start_move_in_seconds'].values[0]
+        predicted_idx_start = ((df['time_milisecond']/1000) - predicted_time_start).abs().idxmin()
+        predicted_x_start = df.loc[predicted_idx_start, 'scaled_rotation_x']
+        predicted_y_start = df.loc[predicted_idx_start, 'scaled_rotation_y']
+        ax.plot(predicted_x_start, predicted_y_start, marker='o', color='orange', label='Start Movement', markersize=8)
+        
+        # Check if there is a stop_turn time for this trajectory
+        if 'stop_turn_in_seconds' in df_predicted.columns and not df_predicted.loc[df_predicted['id'] == idx, 'stop_turn_in_seconds'].isna().all():
+            predicted_time_turn = df_predicted.loc[df_predicted['id'] == idx, 'stop_turn_in_seconds'].values[0]
+            predicted_idx_turn = ((df['time_milisecond']/1000) - predicted_time_turn).abs().idxmin()
+            predicted_x_turn = df.loc[predicted_idx_turn, 'scaled_rotation_x']
+            predicted_y_turn = df.loc[predicted_idx_turn, 'scaled_rotation_y']
+            ax.plot(predicted_x_turn, predicted_y_turn, marker='o', color='red', label='Turning Point', markersize=8)
+        
+        ax.set_xlim(-3, 3)
+        ax.set_ylim(-3, 3)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.legend()
+        ax.set_title(f'Trajectory {idx}')
+    
+    fig.suptitle('Predictions', fontsize=16)
+    plt.tight_layout(rect=[0, 0, 1, 0.96])  # Adjust layout to fit the title
+    plt.show()
+
+
+
+def predict_and_plot_start(df_test_features, models, model_name, features, model_path):
+    df_predicted = make_predictions(df_test_features, models, model_name, features, model_path)
+    edited_df_predicted = edit_predictions(df_predicted, df_test_features, 'start_move')
+    return edited_df_predicted
+
+def predict_and_plot_turn(df_test_features, models, model_name, features, model_path):
+    df_test_features_turn = df_test_features[(~df_test_features['id_update'].isna()) & (df_test_features['id_target'] != df_test_features['id_update'])]
+    df_predicted = make_predictions(df_test_features_turn, models, model_name, features, model_path)
+    edited_df_predicted = edit_predictions(df_predicted, df_test_features_turn, 'stop_turn')
+    return edited_df_predicted
+
+def main():
+    merged_df = convert_raw_to_df('/Users/avitalvasiliev/Documents/GitHub/ExtractingTurningPointGUI/t011222')
+    df_test_features = feature_calc(merged_df)
+
+    start_models = 'start_move_model.pth'
+    start_model_name = {'start_move_model.pth': 'start_move'}
+    start_features = ['scaled_rotation_x', 'scaled_rotation_y','normalized_time','scaled_velocity','scaled_angle']
+    start_model_path = f'/Users/avitalvasiliev/Documents/GitHub/ExtractingTurningPointGUI/{start_models}'
+    start_df_predicted = predict_and_plot_start(df_test_features, start_models, start_model_name, start_features, start_model_path)
+
+    turn_features = ['scaled_rotation_x', 'scaled_rotation_y','normalized_time','scaled_velocity','scaled_angle','scaled_angle_to_end','scaled_curvature','scaled_distance_to_end']
+    turn_models = 'stop_turn_model.pth'
+    turn_model_name = {'stop_turn_model.pth': 'stop_turn'}
+    turn_model_path = f'/Users/avitalvasiliev/Documents/GitHub/ExtractingTurningPointGUI/{turn_models}'
+    turn_df_predicted = predict_and_plot_turn(df_test_features, turn_models, turn_model_name, turn_features, turn_model_path)
+
+    merged_prediction = start_df_predicted.merge(turn_df_predicted, on='id', how='outer')
+    columns_to_drop = [col for col in merged_prediction.columns if col.endswith('_y')]
+    merged_prediction.drop(columns=columns_to_drop, inplace=True)
+    merged_prediction.columns = merged_prediction.columns.str.replace('_x', '', regex=False)
+
+    #TODO: save to file
+    plot_trajectory(merged_prediction, df_test_features)
+
+    return merged_prediction  
